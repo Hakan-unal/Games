d8423fa1d4ea111d6db42e10c76bb360fe2e86c9
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-28 19:02:35 +0300

    initial code

---
+++ Exchange Rate App/exchange.js
@@ -0,0 +1,8 @@
+const currencyOne = document.querySelector("#currency-one");
+const currencyTwo = document.querySelector("#currency-two");
+const inputOne = document.querySelector("#input-one");
+const inputTwo = document.querySelector("#input-two");
+const swapButton = document.querySelector("#button");
+const info = document.querySelector("#date");
+
+console.log(currencyOne, currencyTwo, inputOne, inputTwo, swapButton, info);
