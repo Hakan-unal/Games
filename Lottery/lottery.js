ef62152fc4a40c052fbd7da8b369a6ed1ad5127a
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-29 20:00:46 +0300

    updated

--- Lottery/lottery.js
+++ Lottery/lottery.js
@@ -1,17 +1,23 @@
+// Html dökümanı üzerindeki gerekli elementler yakalandı
 const ticket = document.querySelector("#ticket");
 const lottery = document.querySelector("#lottery");
 const buyTicketButton = document.querySelector("#buyTicketButton");
 const tryButton = document.querySelector("#tryButton");
 const price = document.querySelector("#price");
 
+// hem ticket hem lottery 10 haneli olacağğı için size değişkeni 10'a eşitlendi
 const size = 10;
 let ticketNumbers, lotteryNumbers;
 
+// Kutucukların içerisinde yer alacak sayılar aşağıdaki fonksiyonda yaratıldı ve
+// fonksiyonun çağırıldığı yere retrun edildi
 const randomNumberCreator = () => {
     let value = Math.round(Math.random() * 9);
     return value;
 }
 
+// ticket değişkeninin mevcut değeri "" bu değere eşitlendikten sonra aşağıdaki kod
+// bloğuyla size değişkeni içerisindeki sayı kadar kutucuk oluşturur.
 const createTicketBox = () => {
     ticket.innerHTML = "";
     for (let i = 0; i < size; i++) {
@@ -19,6 +25,11 @@ const createTicketBox = () => {
     }
 }
 
+// for döngüsüyle birlikte size kadar random sayı oluşturulur, html dökümanı üzerinde
+// kutucuk yakalanır ve sayı kutucuğa yerleştirilir bu işlem size değişkeninin
+// içerisindeki sayı kadar devam eder. Fonksiyondan return edilen değer ise kullanıcının
+// kazandığı price değerini hesaplayabilmek için random oluşturulan sayıların tek bir
+// yerde tutulmasi gerektiğinden dolayı ticketArray içinde tutulup return edildi
 const getTicketNumber = () => {
     let ticketArray = [];
     for (let i = 0; i < size; i++) {
@@ -30,6 +41,7 @@ const getTicketNumber = () => {
     return ticketArray;
 }
 
+// lottery için gerekli kutucuklar aşağıdaki kod bloğuyla yaratıldı
 const createLotteryBox = () => {
     lottery.innerHTML = "";
     for (let i = 0; i < size; i++) {
@@ -37,6 +49,11 @@ const createLotteryBox = () => {
     }
 }
 
+// for döngüsüyle birlikte size kadar random sayı oluşturulur, html dökümanı üzerinde
+// kutucuk yakalanır ve sayı kutucuğa yerleştirilir bu işlem size değişkeninin
+// içerisindeki sayı kadar devam eder. Fonksiyondan return edilen değer ise kullanıcının
+// kazandığı price değerini hesaplayabilmek için random oluşturulan sayıların tek bir
+// yerde tutulması gerektiğinden dolayı lotteryArray içinde tutulup return edildi
 const getLotteryNumber = () => {
     let lotteryArray = [];
     for (let i = 0; i < size; i++) {
@@ -48,6 +65,9 @@ const getLotteryNumber = () => {
     return lotteryArray;
 }
 
+// lotteryArray ve ticketArray içerisindeki değerler aynı indiste birbirlerine eşit
+// olursa kullanıcının kazandığı price değeri 25000 arttırılır ve fonksiyonun 
+// çağırıldığı yere count değişkeni return edilir
 const controlPrice = () => {
     let count = 0;
     if (ticketNumbers.length == 10 && lotteryNumbers.length == 10) {
@@ -60,6 +80,7 @@ const controlPrice = () => {
     }
 }
 
+// buyTicketButton değişkenine click event'i gerçekleştiğinde aşağıdaki kod bloğu çalışır
 buyTicketButton.addEventListener("click", () => {
     let ticketArray = [];
     createTicketBox();
@@ -67,6 +88,7 @@ buyTicketButton.addEventListener("click", () => {
     ticketNumbers = ticketArray;
 });
 
+// tryButton değişkenine click event'i gerçekleştiğinde aşağıdaki kod bloğu çalışır
 tryButton.addEventListener("click", () => {
     let award;
     let lotteryArray = [];
