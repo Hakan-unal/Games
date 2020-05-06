484e27b7fdd161ae7e1589a00f7f18cdec3a7836
Hakan ÜNAL <hakanunal_1996@hotmail.com>
2020-05-06 17:01:05 +0300

    updated

--- Segmentify Sample Project/widgets.js
+++ Segmentify Sample Project/widgets.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const contentRow = document.querySelector("#contentrow");
     const cardDeck = document.querySelector("#carddeck");
+    const widget3 = document.querySelector("#widget3");
     const leftClick = document.querySelector("#leftclick");
     const rightClick = document.querySelector("#rightclick");
     let index = 0;
@@ -36,14 +37,14 @@ document.addEventListener("DOMContentLoaded", () => {
                         <img src="${products[i].image}" alt="${products[i].productId}" style="height: 200px; width:100px;">
                         </a>
                         <div class="card-body">
-                            <div class="card-title mt-3"><b>${products[i].name}</b></div>
+                            <div class="card-title mt-3  text-center"><b>${products[i].name}</b></div>
                             <div class="card-text text-muted">
-                                <span>${products[i].categories[0]} | ${products[i].categories[1]} | ${products[i].categories[2]}</span>     
+                                <p class="text-center">${products[i].categories[0]} | ${products[i].categories[1]} | ${products[i].categories[2]}</p>     
                             </div>
-                            <div class="card-footer bg-white border-0 mt-4"><p>
+                            <div class="card-footer bg-white border-0 mt-4"><p class=" text-center">
                             <b>${products[i].priceText}</b> <span class="text-danger"><del>${products[i].oldPriceText}</del></span> 
                             </p>
-                            <span class="text-muted">${products[i].params.basePrice}</span></div>
+                            <p class="text-muted  text-center">${products[i].params.basePrice}</p></div>
                         </div>
                     </div>
                 </div>
@@ -67,9 +68,9 @@ document.addEventListener("DOMContentLoaded", () => {
                     <img class="" src="${products[i].image}" alt="${products[i].productId}" style="height:225px; width:100px;">
                 </a>
                 <div class="card-body">
-                        <h6 class="card-title"> <b>${products[i].name} </b></h6>
+                        <div class="card-title text-center"> <b>${products[i].name} </b></div>
                         <div class="card-text text-muted">
-                            <span>${products[i].categories[0]} | ${products[i].categories[1]} | ${products[i].categories[2]}</span>     
+                            <p class="text-center">${products[i].categories[0]} | ${products[i].categories[1]} | ${products[i].categories[2]}</p>     
                         </div>
                     </div>
                     <div class="card-footer border-0 text-center">
@@ -82,12 +83,41 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    setInterval(() => {
-        cardDeck.innerHTML = "";
-        getData2(Math.round(Math.random() * 27));
 
-    }, 2500);
-    getData2(0);
+
+    getData3 = (index) => {
+        fetchProductsData().then(text => {
+            const products = text.products;
+            console.log(products);
+
+            for (let i = index; i < index + 6; i++) {
+                widget3.innerHTML += `
+                <div class="col-sm-6 col-md-3 col-lg-2 mt-4">
+                    <div class="card">
+                        <a class="d-flex justify-content-center" href="${products[i].url}" target="_blank">
+                        <img class="" src="${products[i].image}" alt="${products[i].productId}" style="height:225px; width:100px;">
+                        </a>
+                        <div class="card-body">
+                            <div class="card-title text-center"> <b>${products[i].name} </b></div>
+                            <div class="card-text text-muted">
+                                <p class="text-center">${products[i].categories[0]} | ${products[i].categories[1]} | ${products[i].categories[2]}</p>     
+                            </div>
+
+                        </div>
+                        <div class="card-footer border-0 text-center">
+                            <span class="text-success h3">${products[i].priceText}</span>
+                            <small class="text-danger h5"><del>${products[i].oldPriceText} </del></small>
+                        </div>
+                    </div>
+                </div>
+                `
+            }
+
+
+        })
+    }
+
+
 
 
 
@@ -119,7 +149,20 @@ document.addEventListener("DOMContentLoaded", () => {
     leftClick.addEventListener("click", leftClickFunction);
     rightClick.addEventListener("click", rightClickFunction);
 
+
+
+
     getData1(index);
+    getData2(0);
+    getData3(0);
+
+    setInterval(() => {
+        cardDeck.innerHTML = "";
+        getData2(Math.round(Math.random() * 26));
+
+    }, 2500);
+
+
 
 
 
