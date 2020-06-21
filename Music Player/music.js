74dab2dc5053f687e7e736cc0f683d6b58248077
Hakan-unal <hakanunal_1996@hotmail.com>
2020-06-21 12:43:31 +0300

    updated

--- Music Player/music.js
+++ Music Player/music.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const backwardIcon = document.querySelector("#backwardIcon");
     const playpauseIcon = document.querySelector("#playpauseIcon");
     const forwardIcon = document.querySelector("#forwardIcon");
-
+    const progresBar = document.querySelector("#progresBar");
 
     playpauseIcon.addEventListener("click", () => {
 
@@ -15,19 +15,25 @@ document.addEventListener("DOMContentLoaded", () => {
         } else if (playpauseIcon.className === "far fa-play-circle fa-3x") {
             playpauseIcon.setAttribute("class", "far fa-pause-circle fa-3x");
             audio.play();
-
         }
     })
 
     audio.addEventListener("play", () => {
         icon.setAttribute("class", "fab fa-napster fa-6x fa-spin")
-        console.log(audio.duration);
-        console.log(audio.currentTime)
     })
+
+
 
     audio.addEventListener("pause", () => {
         icon.setAttribute("class", "fab fa-napster fa-6x")
     })
+
+
+    setInterval(() => {
+        if (audio.currentTime !== 0) {
+            progresBar.setAttribute("style", `background-color: black; width:${audio.currentTime/audio.duration*100}px;`);
+        }
+    }, 1000);
 })
 
 
