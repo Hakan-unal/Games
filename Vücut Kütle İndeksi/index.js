09514aa0c1bea126aa64b78fe0ee050bb24f4d9c
Hakan-unal <hakanunal_1996@hotmail.com>
2020-07-15 13:02:07 +0300

    updated

---
+++ Vücut Kütle İndeksi/index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Man Elements
+    const manWeight = document.querySelector("#manWeight");
+    const manHeight = document.querySelector("#manHeight");
+    const manResultButton = document.querySelector("#manResultButton");
+    const manResult = document.querySelector("#manResult");
+    const manForm = document.querySelector("#manForm");
+
+    // Women Elements
+    const womanWeight = document.querySelector("#womanWeight");
+    const womanHeight = document.querySelector("#womanHeight");
+    const womanResultButton = document.querySelector("#womanResultButton");
+    const womanResult = document.querySelector("#womanResult");
+    const womenForm = document.querySelector("#womanForm");
+
+
+
+    const manCalculator = (weight, height) => {
+        console.log(weight, height);
+    }
+
+
+
+
+    manForm.addEventListener("submit", (event) => {
+        const weight = Number(manWeight.value);
+        const height = Number(manHeight.value);
+
+        manCalculator(weight, height);
+
+        event.preventDefault();
+    })
+
+    womanForm.addEventListener("submit", (event) => {
+        console.log("Woman Form Submit")
+        event.preventDefault();
+    })
+
+
+
+})
